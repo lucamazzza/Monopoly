0e03b13f2bf98d0f01f32f84386cab789c9f4d75
Ivo Herceg <ivo.herceg@student.supsi.ch>
2024-03-01 09:45:22 +0100

    feat(#5): class Dice, + constants
    Dice class created
    method's added: roll, print
    added MIN/MAX dice value in Constant class

---
+++ src/Dice.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+public class Dice {
+    private int loBound;
+    private int hiBound;
+    private final Random random = new Random();
+
+    public Dice(int loBound, int hiBound) {
+        this.loBound = loBound;
+        this.hiBound = hiBound;
+    }
+    public int roll(){
+        return random.nextInt(loBound,hiBound + 1);
+    }
+    public void print(){
+        System.out.println("You rolled: " + roll());
+    }
+}
