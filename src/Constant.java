7080a03d2994d62da4e3d7fc33f75f4d1f09cb04
AndreaMasciocchi <andrea.masciocchi@samtrevano.ch>
2024-03-01 08:56:31 +0100

    fix(#7): made impossible to instantiate Constant class

--- src/Constant.java
+++ src/Constant.java
@@ -4,4 +4,8 @@ public final class Constant {
     public static final int PLAYER_NUMBER = 2;
     public static final int DICE_NUMBER = 1;
     public static final int DICE_FACES = 4;
+
+    private Constant(){
+        throw new IllegalStateException("Utility Class");
+    }
 }
