bd084d9619ce95a757c649dd74b1c4a5ff822f49
AndreaMasciocchi <andrea.masciocchi@samtrevano.ch>
2024-03-01 08:52:33 +0100

    feat(#5,#7): constants for dice created

--- src/Constant.java
+++ src/Constant.java
@@ -2,4 +2,6 @@ public final class Constant {
     public static final int BANK_START_AMOUNT = 1000000;
     public static final int PLAYER_START_AMOUNT = 2000;
     public static final int PLAYER_NUMBER = 2;
+    public static final int DICE_NUMBER = 1;
+    public static final int DICE_FACES = 4;
 }
