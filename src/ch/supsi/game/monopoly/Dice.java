232db313e00107103663db360378afa1779ebe22
Luca Mazza <luca.mazza@student.supsi.ch>
2024-03-05 22:43:51 +0100

    refactor(#5): modified print function to comply with the standard overridden `toString` from parent class `Object`.
    Co-authored-by: Ivo Herceg <ivo.herceg@student.supsi.ch>
    Signed-off-by: Luca Mazza <luca.mazza@student.supsi.ch>

--- src/ch/supsi/game/monopoly/Dice.java
+++ src/ch/supsi/game/monopoly/Dice.java
@@ -6,14 +6,19 @@ public class Dice {
     private int hiBound;
     private final Random random = new Random();
 
+    private int currentValue;
+
     public Dice(int loBound, int hiBound) {
         this.loBound = loBound;
         this.hiBound = hiBound;
     }
     public int roll(){
         return random.nextInt(loBound,hiBound + 1);
+    public void roll(){
+        this.currentValue = random.nextInt(loBound,hiBound + 1);
     }
     public void print(){
         System.out.println("You rolled: " + roll());
+
     }
 }
