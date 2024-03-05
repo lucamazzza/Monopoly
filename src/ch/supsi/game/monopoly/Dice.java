5d5b8616242370bb11a2195a59680fd042ca96a9
Mazza Luca <luca.mazza@student.supsi.ch>
2024-03-05 21:48:59 +0000

    fix: IntelliJ Bug uncommitted changes but changed anyway on IDE

--- src/ch/supsi/game/monopoly/Dice.java
+++ src/ch/supsi/game/monopoly/Dice.java
@@ -5,7 +5,6 @@ public class Dice {
     private int loBound;
     private int hiBound;
     private final Random random = new Random();
-
     private int currentValue;
 
     public Dice(int loBound, int hiBound) {
@@ -17,8 +16,8 @@ public class Dice {
     public void roll(){
         this.currentValue = random.nextInt(loBound,hiBound + 1);
     }
-    public void print(){
-        System.out.println("You rolled: " + roll());
-
+    @Override
+    public String toString() {
+        return String.valueOf(this.currentValue);
     }
 }
