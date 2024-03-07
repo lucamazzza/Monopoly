95cd8de0f873ec3c73ef961fd07135f86af68cc5
Luca Mazza <luca.mazza@student.supsi.ch>
2024-03-07 08:54:19 +0100

    feat(#3): bank withdrawal (give money to player) and deposit (player pay something to bank) methods
    Signed-off-by: Luca Mazza <luca.mazza@student.supsi.ch>
    Co-authored-by: Ivo Herceg <ivo.herceg@student.supsi.ch>
    Signed-off-by: Luca Mazza <luca.mazza@student.supsi.ch>

--- src/ch/supsi/game/monopoly/Bank.java
+++ src/ch/supsi/game/monopoly/Bank.java
@@ -4,13 +4,30 @@ public class Bank {
     private int amount;
     public Bank(){
         this.amount = Constant.BANK_START_AMOUNT;
+    public int getBalance() {
+        return balance;
     }
 
     public int getAmount() {
         return amount;
+    public void deposit(int amount) {
+        if (amount < 1) {
+            return;
+        }
+        this.balance += amount;
     }
 
     public void setAmount(int amount) {
         this.amount = amount;
+    public void withdraw(int amount){
+        if (amount < 1) {
+            return;
+        }
+        this.balance -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bank: %d.–", this.balance);
     }
 }
